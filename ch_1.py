714a1740073632da64346e8f6028e7241ff7692a
RonKbS <stephen.bamwite@gmail.com>
2019-02-03 14:07:29 +0300

    load the data

--- ch_1.py
+++ ch_1.py
@@ -1,5 +1,22 @@
+from __future__ import absolute_import, division, print_function
 import tensorflow as tf
 from tensorflow.keras import layers
 
 print(tf.VERSION)
 print(tf.keras.__version__)
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
