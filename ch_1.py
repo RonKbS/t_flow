eeb899f674b217dfcc4070715206508abf1e4691
RonKbS <stephen.bamwite@gmail.com>
2019-05-27 16:15:27 +0300

    test accuracy

--- ch_1.py
+++ ch_1.py
@@ -25,27 +25,49 @@ fashion_mnist = keras.datasets.fashion_mnist
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-print(train_images.shape)
+# print(train_images.shape)
 
-print(len(train_labels))
+# print(len(train_labels))
 
-print(train_labels)
+# print(train_labels)
 
-print(test_images.shape)
+# print(test_images.shape)
 
 
-print(len(test_labels))
+# print(len(test_labels))
 
-plt.figure()
-plt.imshow(train_images[50], dtype='unit64')
-plt.colorbar()
-plt.grid(False)
-plt.show()
-
-# data = [[0, 0.25], [0.5, 0.75]]
-
-# fig, ax = plt.subplots()
-# im = ax.imshow(data, cmap=plt.get_cmap('hot'), interpolation='nearest',
-#                vmin=0, vmax=1)
-# fig.colorbar(im)
+# plt.figure()
+# plt.imshow(train_images[0] / 255.0)
+# plt.colorbar()
+# plt.grid(False)
 # plt.show()
+
+train_images = train_images / 255.0
+
+test_images = test_images / 255.0
+
+# plt.figure(figsize=(10,10))
+# for i in range(25):
+#     plt.subplot(5,5,i+1)
+#     plt.xticks([])
+#     plt.yticks([])
+#     plt.grid(False)
+#     plt.imshow(train_images[i], cmap=plt.cm.binary)
+#     plt.xlabel(class_names[train_labels[i]])
+# plt.show()
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+model.compile(optimizer='adam', 
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=5)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print('Test accuracy:', test_acc)
